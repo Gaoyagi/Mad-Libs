8ddfaba0c5025ecdb37274496de3fa98f207e3dd
George Aoyagi <aoyagigeorge@gmail.com>
2019-08-29 15:09:36 -0700

    first full draft of hw

--- madLibs.py
+++ madLibs.py
@@ -1,9 +1,18 @@
 #Mad libs project
-libs = ["occupation": "___", "noun0":"___", "verb0":"___", "adjective0": "___", "noun1": "___", "verb1": "___", "adjective1": "___", "noun2": "____", "verd2":"___", "noun3":"____"]
+libs = ["occupation": "occupation", "noun0":"noun", "verb0":"verb", "adjective0":"adjective", "noun1":"noun", "verb1": "verb", "adjective1":"adjective", "noun2":"noun", "verb2":"verb", "noun3":"noun"]
 
 #Mad libs template taken from glowwordbooks.com
 madlib = """Today a %0s named %1s came to our school to talk to us about her job.
             She said the most important skill you need to know to do her job is to be able to %2s around (a) %3s %4.
             She said it was easy for her to learn her job because she loved to %5s when she was my age--and that helps a lot!
             If you're considering her profession, I hope you can be near (a) %6s %7s. That's very important!
-            If you get too distracted in that situation you won't be able to %8s next to (a) %s9!""" %(libs)
+            If you get too distracted in that situation you won't be able to
+            %8s next to (a) %s9!""" %(libs["occupation"], libs["noun0"], libs["verb0"],libs["adjective0"], libs["noun1"], libs["verb1"], libs["adjective1"], libs["noun2"], libs["verb2"], libs["noun3"])
+
+def inputs():
+    for words in libs:
+        temp = input("enter a ", libs[words])
+        libs[words] = temp
+
+inputs()
+print madlib
