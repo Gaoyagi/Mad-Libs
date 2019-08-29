f08d46f53c9bfd7ec641f60e8bf35438a9c1bbdb
George Aoyagi <aoyagigeorge@gmail.com>
2019-08-29 14:56:39 -0700

    added dictionary and madlibs template

--- madLibs.py
+++ madLibs.py
@@ -1,1 +1,9 @@
 #Mad libs project
+libs = ["occupation": "___", "noun0":"___", "verb0":"___", "adjective0": "___", "noun1": "___", "verb1": "___", "adjective1": "___", "noun2": "____", "verd2":"___", "noun3":"____"]
+
+#Mad libs template taken from glowwordbooks.com
+madlib = """Today a %0s named %1s came to our school to talk to us about her job.
+            She said the most important skill you need to know to do her job is to be able to %2s around (a) %3s %4.
+            She said it was easy for her to learn her job because she loved to %5s when she was my age--and that helps a lot!
+            If you're considering her profession, I hope you can be near (a) %6s %7s. That's very important!
+            If you get too distracted in that situation you won't be able to %8s next to (a) %s9!""" %(libs)
